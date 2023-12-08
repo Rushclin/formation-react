e55f82ffac93209649de089df2986661bbd7daca
Rushclin Takam <takamrushclin@gmail.com>
2023-12-08 08:24:41 +0100

    Edit mock api

--- src/__mock__/fakedb/products/index.ts
+++ src/__mock__/fakedb/products/index.ts
@@ -2,15 +2,15 @@ import { ProduitType } from "@/types/produits";
 
 export const produitDatas: ProduitType[] = [
   {
-    image: "/jacket-1.jpg",
+    image: "/images/produits/jacket-1.jpg",
     nom: "Jacket",
-    description: "MEN Yarn Fleece Full-Zip Jacket",
-    prix: 45,
+    description: "Jacket pour homme avec un res grand col, commandez le.",
+    prix: 4500,
     id: 1,
     tva: 10,
   },
   {
-    image: "/skirt-1.jpg",
+    image: "/images/produits/skirt-1.jpg",
     nom: "Skirt",
     description: "Black Floral Wrap Midi Skirt",
     prix: 55,
@@ -18,7 +18,7 @@ export const produitDatas: ProduitType[] = [
     tva: 10,
   },
   {
-    image: "/party-wear-1.jpg",
+    image: "/images/produits/party-wear-1.jpg",
     nom: "Party Wear",
     description: "Women's Party Wear Shoes",
     prix: 25,
@@ -26,7 +26,7 @@ export const produitDatas: ProduitType[] = [
     tva: 10,
   },
   {
-    image: "/shirt-1.jpg",
+    image: "/images/produits/shirt-1.jpg",
     nom: "Shirt",
     description: "Pure Garment Dyed Cotton Shirt",
     prix: 45,
@@ -34,7 +34,7 @@ export const produitDatas: ProduitType[] = [
     tva: 10,
   },
   {
-    image: "/sports-1.jpg",
+    image: "/images/produits/sports-1.jpg",
     nom: "Sports",
     description: "Trekking & Running Shoes - Black",
     prix: 58,
@@ -42,7 +42,7 @@ export const produitDatas: ProduitType[] = [
     tva: 10,
   },
   {
-    image: "/watch-1.jpg",
+    image: "/images/produits/watch-1.jpg",
     nom: "Watches",
     description: "Smart Watches Vital Plus",
     prix: 100,
@@ -50,11 +50,19 @@ export const produitDatas: ProduitType[] = [
     tva: 10,
   },
   {
-    image: "/watch-2.jpg",
+    image: "/images/produits/watch-2.jpg",
     nom: "Watches",
     description: "Pocket Watch Leather Pouch",
     prix: 120,
     id: 7,
     tva: 10,
   },
+  {
+    description: "Trousse de maquillage complet", 
+    id: 8, 
+    image: "https://www.si.edu/sites/default/files/newsdesk/press_releases/cosmetics_market_beauty_products_vietnam_6.jpg", 
+    nom: "Trousse de maquillage", 
+    prix: 5000, 
+    tva: 5
+  }
 ];
